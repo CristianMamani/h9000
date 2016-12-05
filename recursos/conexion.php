7246dd07f6c3899ca34a2d86f7cbfaa5539ab0d4
CristianMamani <cristian_preof@hotmail.com>
2016-12-05 02:33:50 -0400

    Mas errores
    Errores de Syntaxis

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,17 +1,14 @@
 <?php 
 
-global $host, $user, $pass, $database;
+global $host,$user,$pass,$database;
 
 $host='mysql.hostinger.es';
+$user="u296913487_root";
+$pass="desconocido1";
+$database="u296913487_cel";
 
-$user='u296913487_root';
+@$link=mysqli_connect($host,$user,$pass) or die ("conexión fallida");
 
-$pass='desconocido1';
-
-$database='u296913487_cel';
-
-@$link=mysqli_connect($host,$user,$pass) or die ("conexión fallida.");
-
-mysql_select_db($link,$database) or die ("error al conectarse con la base de datos".mysqli_error($link));
+mysqli_select_db($link,$database) or die ("error al conectarse con la base de datos".mysqli_error($link));
 
 ?>
