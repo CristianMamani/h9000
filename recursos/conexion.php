0044567e4ddd7a3e61667a214fdf1d39796a58d0
CristianMamani <cristian_preof@hotmail.com>
2016-12-06 05:33:39 -0400

    Cambio en  "@link"
    Pequeño cambio en "@link"

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,13 +1,13 @@
 <?php 
 
-global $host,$user,$pass,$database;
+global $host,$user,$pass,$database,$link;
 
 $host="mysql.hostinger.es";
 $user="u296913487_root";
 $pass="desconocido1";
 $database="u296913487_cel";
 
-@$link=mysqli_connect($host,$user,$pass) or die ("conexión fallida");
+$link=mysqli_connect($host,$user,$pass) or die ("conexión fallida");
 
 mysqli_select_db($link,$database) or die ("error al conectarse con la base de datos".mysqli_error($link));
 
