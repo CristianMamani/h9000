47396b8376d9d1e311d5124c66babfd741887d3e
CristianMamani <cristian_preof@hotmail.com>
2016-12-06 06:22:00 -0400

    mysqli_select_db() al revez
    Argumentos al revéz

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 
 $conexion = mysqli_connect('mysql.hostinger.es', 'u296913487_root','desconocido1');
-mysqli_select_db('u296913487_cel', $conexion);
+mysqli_select_db($conexion,'u296913487_cel');
 
 ?>
